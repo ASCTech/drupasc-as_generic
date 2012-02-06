f6a809d5487256e7411b3dd83fa92a2815189c9b
Mike Butsko <butsko.7@osu.edu>
2012-02-06 13:46:44 -0500

    Fixed image paths.

--- icons/uicons_basic.php
+++ icons/uicons_basic.php
@@ -1,14 +1,14 @@
 <p>
 
 <?php if(user_is_logged_in()) { ?>
-	<a href="http://jigsaw.w3.org/css-validator/check/referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="<?=base_path();?>sites/all/files/icons/w3c_css.png" height="13px" title="Validate CSS" alt="Validate CSS" /></a>
-	<a href="http://validator.w3.org/check?uri=referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="<?=base_path();?>sites/all/files/icons/w3c_xhtml.png" height="13px" title="Validate XHTML" alt="Validate XHTML" /></a>
+	<a href="http://jigsaw.w3.org/css-validator/check/referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="<?php print base_path() . path_to_theme();?>/icons/w3c_css.png" height="13px" title="Validate CSS" alt="Validate CSS" /></a>S
+	<a href="http://validator.w3.org/check?uri=referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="<?php print base_path() . path_to_theme();?>/icons/w3c_xhtml.png" height="13px" title="Validate XHTML" alt="Validate XHTML" /></a>
 <?php } ?>
 
 <?php if(!user_is_logged_in()) { ?>
-	<a href="<?=base_path();?>user/login?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="<?=base_path();?>sites/all/files/icons/user.gif" height="13px" title="Site Administrator Login" alt="Site Administrator Login" /></a>
+	<a href="<?=base_path();?>user/login?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="<?php print base_path() . path_to_theme();?>/icons/user.gif" height="13px" title="Site Administrator Login" alt="Site Administrator Login" /></a>
 <?php } else { ?>
-	<a href="<?=base_path();?>logout?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="<?=base_path();?>sites/all/files/icons/user_out.gif" height="13px" title="Logout" alt="Logout" /></a>
+	<a href="<?=base_path();?>logout?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="<?php print base_path() . path_to_theme();?>/icons/user_out.gif" height="13px" title="Logout" alt="Logout" /></a>
 <?php } ?>
 </p>
 
